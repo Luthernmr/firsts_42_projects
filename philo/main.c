710b0f33785ff9bc9aca2488adf4d83af4844504
Luther Nemor <lnemor@z1r1p4.42lyon.fr>
2022-02-06 10:09:26 +0100

    luhter

--- philo/main.c
+++ philo/main.c
@@ -6,48 +6,63 @@
 /*   By: lnemor <lnemor@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:04:34 by lnemor            #+#    #+#             */
-/*   Updated: 2022/02/01 16:54:53 by lnemor           ###   ########lyon.fr   */
+/*   Updated: 2022/02/04 16:54:07 by lnemor           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*take_fork(void *data)
+void	*take_fork(t_philo *philo)
 {
-	int	philo;
-
-	philo = (int)data;
-	printf("philo %d prend une fourchette\n", philo);
+	pthread_mutex_lock(&philo[philo->id].mutex_fork_left);
+	printf("philo %d prend une fourchette gauche\n", (int)philo[philo->id].id);
+	pthread_mutex_lock(&philo[philo->id].mutex_fork_right);
+	printf("philo %d prend une fourchette droite\n", (int)philo[philo->id].id);
 	return (NULL);
 }
 
-void	philo_routine()
+void	*routine_philo(void *set)
 {
-	
+	t_setup			*setup;
+
+	setup = (t_setup *)set;
+	take_fork(setup.philo[philo->id]);
+	return (NULL);
 }
+
+//void	*do_eat(void *data)
 
 int	main(int argc, char **argv)
 {
 	int			i;
 	int			res;
-	int			nb;
 	t_setup		setup;
 
 	res = 0;
 	i = 0;
 	if (argc != 2)
 		return (0);
-	philo.nb_of_philo = ft_atoi(argv[1]));
-	while (i < philo.nb_of_philo)
+	setup.number_of_philosophers = ft_atoi(argv[1]);
+	setup.philo = malloc (sizeof(t_philo) * setup.number_of_philosophers);
+	pthread_mutex_init(&setup.mutex_fork, NULL);
+	while (i < setup.number_of_philosophers)
 	{
-		setup.philo[i] = pthread_create 
-	}
-	i = 0;
-	while (i < philo.nb_of_philo)
-	{
-		pthread_join(philo.thread_philo, 0);
+		setup.philo[i].id = i + 1;
+		setup.philo[i].eat = 0;
+		setup.philo[i].sleep = 0;
 		i++;
 	}
+	i = 0;
+	while (i < setup.number_of_philosophers)
+	{
+		pthread_create(&(setup.philo[i].thread_id), NULL,
+			&routine_philo,
+			(void *)setup.philo);
+		setup.philo[i].id = i + 1;
+		usleep(200);
+		i++;
+	}
+
 	//eat = pthread_create(fork, NULL, take_fork, "test");
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: lnemor <lnemor@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:20:22 by lnemor            #+#    #+#             */
-/*   Updated: 2022/02/01 16:47:47 by lnemor           ###   ########lyon.fr   */
+/*   Updated: 2022/02/04 15:26:20 by lnemor           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,25 @@
 
 typedef struct t_philo
 {
-	int			id;
-	pthread_t	thread_philo;
+	size_t			id;
+	int				eat;
+	int				sleep;
+	int				die;
+	pthread_mutex_t	mutex_fork_right;
+	pthread_mutex_t	mutex_fork_left;
+	pthread_t		thread_id;
+
 }				t_philo;
 
 typedef struct t_setup
 {
-	int			nb_of_philo;
-	t_philo		*philo;
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				time_must_eat;
+	t_philo			*philo;
+	pthread_mutex_t	mutex_fork;
 }				t_setup;
 
 #endif
